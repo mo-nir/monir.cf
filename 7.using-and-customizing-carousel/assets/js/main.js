d7f1ec71d226ca5bde0cb4923059aec49e1edd34
Monir <monir.rj@gmail.com>
2017-09-29 23:17:35 +0600

    class-7

---
+++ 7.using-and-customizing-carousel/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('.owl-carousel').owlCarousel({
+            items:4,
+            loop:true,
+            margin:30,
+            autoplay:true,
+            nav:true,
+            navText:['<i class="fa fa-long-arrow-right"></i>','<i class="fa fa-long-arrow-right"></i>']
+        })
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
