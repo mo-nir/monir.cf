cb2ecd9dd29a0307452ff933f701b46f3d15f259
Monir <monir.rj@gmail.com>
2017-09-30 21:54:03 +0600

    class-9

---
+++ 9.testimonial-carousel-with-customized-owl-dots/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".owl-carousel").owlCarousel({
+            items:3,
+            margin: 15,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: true,
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
